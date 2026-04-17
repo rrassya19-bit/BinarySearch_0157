1e06709338b3f8bf0894cbb1786f0db650f3439e
rrassya19-bit <rrassys19@gmail.com>
2026-04-17 15:46:11 +0700

    fungsi input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+int element [10];
+int nPanjang;
+int X;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array (Maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
